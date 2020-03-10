cc4695d85cbca9252c15c8e114f30f8504a0558c
ArtemchikNOT <artemchuro@mail.ru>
2020-03-10 20:58:02 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-
-
 exports.cToF = function (c) {
-
+    f = c * 1.8 + 32;
+    return ((f).toFixed(2));
 }
 
 exports.fToC = function (f) {
-
+    c = (f - 32) / (18 / 10);
+    return c.toFixed(2);
 }
